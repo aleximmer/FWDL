a39e8e835ad26d66247400ea594b19db30c36ffa
Alexander Immer <alexndr.immer@gmail.com>
2018-05-23 10:51:39 +0200

    Add oracles

---
+++ oracles.py
@@ -0,0 +1,14 @@
+import numpy as np 
+from scipy.sparse.linalg import svds
+
+
+def LMO_l1(grad, kappa):
+    s = np.zeros(grad.shape)
+    coord = np.argmax(np.abs(grad))
+    s[coord] = kappa * np.sign(grad[coord])
+    return - s * coord
+
+
+def LMO_nuclear(grad, kappa):
+    u, s, vt = svds(grad, k=1, which='LM')
+    return - kappa * np.outer(u, vt)
