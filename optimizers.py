c32db2bac11b7957cb5f539782b65016013fe3b1
Alexander Immer <alexndr.immer@gmail.com>
2018-05-23 12:42:03 +0200

    Fix superclassing

--- optimizers.py
+++ optimizers.py
@@ -7,7 +7,7 @@ class SGDl1(Optimizer):
     def __init__(self, params, lr, lambda_l1):
         assert lambda_l1 > 0
         defaults = dict(lr=lr, l1=lambda_l1)
-        super(Optimizer, self).__init__(params, defaults)
+        super(SGDl1, self).__init__(params, defaults)
 
     def step(self, closure=None):
         """Performs a single optimization step.
@@ -35,9 +35,47 @@ class SGDl1(Optimizer):
 
 
 class SGDFWl1(Optimizer):
+    """Stochastic Frank Wolfe with |vec(W_i)|_1 <= kappa_l1 where W_i are parameter sets
+    """
     def __init__(self, params, kappa_l1):
         self.k = 0
-        assert kappa > 0
+        assert kappa_l1 > 0
+        defaults = dict(kappa=kappa_l1)
+        super(SGDFWl1, self).__init__(params, defaults)
+
+    def step(self, closure=None):
+        """Performs a single optimization step.
+
+        Arguments:
+            closure (callable, optional): A closure that reevaluates the model
+                and returns the loss.
+        """
+        loss = None
+        if closure is not None:
+            loss = closure()
+
+        for group in self.param_groups:
+            kappa = group['kappa_l1']
+
+            for p in group['params']:
+                if p.grad is None:
+                    continue
+                s = LMO_l1(p.grad.data.numpy(), kappa)
+                gamma = 2 / (self.k + 2)
+                # x^(k+1) = x^(k) - g x^(k) + g s
+                delta_p = gamma * s - gamma * p.data
+                p.data.add_(delta_p)
+
+        self.k += 1
+        return loss
+
+
+class SGDFWNuclear(Optimizer):
+    """Stochastic Frank Wolfe with |W_i|_* <= kappa_l1 where W_i are parameter sets
+    """
+    def __init__(self, params, kappa_l1):
+        self.k = 0
+        assert kappa_l1 > 0
         defaults = dict(kappa=kappa_l1)
         super(Optimizer, self).__init__(params, defaults)
 
@@ -58,14 +96,11 @@ class SGDFWl1(Optimizer):
             for p in group['params']:
                 if p.grad is None:
                     continue
-                d_p = p.grad.data
-                s = LMO_l1(d_p, kappa)
+                s = LMO_nuclear(p.grad.data.numpy(), kappa)
                 gamma = 2 / (self.k + 2)
-                
-
-                #d_p.add_(lam, torch.sign(p.data))
-
-                p.data.add_(-group['lr'], d_p)
+                # x^(k+1) = x^(k) - g x^(k) + g s
+                delta_p = gamma * s - gamma * p.data
+                p.data.add_(delta_p)
 
         self.k += 1
         return loss
